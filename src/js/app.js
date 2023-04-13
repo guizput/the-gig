0bcc7d3c4f6618dbb9cce35e74c7d39d844ec03e
Guillaume Duran <guillaumeduran2@gmail.com>
2023-04-13 13:43:40 +0000

    refacto: install TailwindCSS

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import Nav from "./Nav.js";
-
-let nav = new Nav({
-  toggle: ".nav__toggle",
-  links: ".nav__links",
-  linksActiveClass: "nav__links--active",
-  toggleActiveClass: "nav__toggle--active",
-});
